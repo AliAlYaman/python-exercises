9b5eb55cddc8a548a52f7cda224ae10b0b1eec9a
Ali Al Yaman <alialyaman750@gmail.com>
2024-11-19 12:44:30 +0200

    Solved array rotation

---
+++ Arrays/Easy/rotate_right.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/rotate-array/submissions/1457147969/
+# Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,4,5,6,7], k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+
+# Input: nums = [-1,-100,3,99], k = 2
+# Output: [3,99,-1,-100]
+# Explanation: 
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+def rotate(nums, k):
+        n = len(nums)
+        k = k % n 
+        return nums[-k:] + nums[:-k]
+
+
+
+print(rotate([1,2,3,4,5,6,7], 3))
